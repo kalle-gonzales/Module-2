4399f24a11c6175ee44426cc843475c7d0d9d7cc
Kilian Mauer <kilian.mauer@buchhalter.pro>
2020-04-24 15:36:34 +0200

    taking a break

---
+++ javascript/add_recipe.js
@@ -0,0 +1,100 @@
+const add_btn            = document.getElementById("add_btn"),
+      add_ingredient_btn_0 = document.getElementById("add_ingredient_btn_0"),
+      ingredient_limit   = 30,
+      step_limit         = 100;
+var   readout            = JSON.parse(localStorage.getItem("recipes")),
+      recipes            = readout ? readout : [],
+      ingredient_counter = 0;
+
+console.log(recipes);
+
+add_btn.addEventListener("click", function(event){
+  event.preventDefault();
+  let input                = [],
+      ingredients          = [],
+      instructions         = [],
+      ingredients_to_push  = false,
+      instructions_to_push = false;
+
+  Array.from(document.forms["new_recipe"]).forEach(function(element){
+    if (element.classList.contains("ingredient")) {
+      ingredients_to_push = true;
+      ingredients.push(element.value);
+    } else if (element.classList.contains("instruction")) {
+      instructions_to_push = true;
+      instructions.push(element.value);
+    } else {
+      if (ingredients_to_push){
+        input.push(JSON.stringify(ingredients));
+        ingredients_to_push = false;
+      } else if (instructions_to_push) {
+        input.push(JSON.stringify(instructions));
+        instructions_to_push = false;
+      };
+      input.push(element.value);
+    }
+  });
+  recipes.push(input);
+  window.localStorage.setItem("recipes", JSON.stringify(input));
+})
+
+
+addEventListeners(
+  document.getElementById("ingredient_add_btn_0"),
+  "ingredient_",
+  "Zutat ",
+  1,
+  ingredient_limit,
+  "So viel Zeug brauchst Du nicht, Paul Bucouse!"
+);
+
+addEventListeners(
+  document.getElementById("step_add_btn_0"),
+  "step_",
+  "Schritt ",
+  1,
+  step_limit,
+  "Das wird zu kompliziert..."
+);
+
+function addEventListeners(dom, category, text, counter, limit, limit_text){
+  dom.addEventListener("click", function(){
+    if (counter <= limit) {
+      console.log("input_box_" + category + (counter-1).toString());
+      var div   = document.getElementById("input_box_" + category + (counter-1).toString()),
+          clone = div.cloneNode(true);
+      clone.id = "input_box_" + category + counter;
+      clone.childNodes[1].innerHTML = text + (counter+1);
+      clone.childNodes[3].id        = category + counter;
+      clone.childNodes[5].id        = category + "add_btn_" + counter;
+      clone.childNodes[7].id        = category + "remove_btn_" + counter;
+
+
+      div.parentNode.insertBefore(clone, div.nextSibling);
+
+      addEventListeners(clone.childNodes[5], category, text, counter + 1, limit, limit_text);
+
+      let remove = document.getElementById(category + "remove_btn_" + (counter-1).toString());
+      remove.classList.remove("fa_hidden");
+
+      remove.addEventListener("click", function(){
+        this.parentElement.remove();
+        // todo: adjust numbers...
+      })
+
+      console.log(dom.childNodes);
+    } else {
+      alert(limit_text);
+    }
+    this.style.display = "none";
+  });
+};
+
+
+
+
+document.onkeypress = function(event){
+  if (event.ctrlKey && event.key == "p"){
+    console.log("hi");
+  }
+};
