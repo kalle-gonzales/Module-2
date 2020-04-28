5d844961ed47e35109ef61c64afa785a91ca85b7
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2020-04-28 15:00:14 +0200

    cleaned up version
    cleaned unused code and added comments

--- javascript/alleRezepte.js
+++ javascript/alleRezepte.js
@@ -13,119 +13,48 @@ btn_dessert. addEventListener("click", foodFilter);
 btn_drink.   addEventListener("click", foodFilter);
 btn_Alle.    addEventListener("click", foodFilter);
 
-//LOAD.addEventlistener("onload", foodFilter);
-
-// let url =  window.location.href
-// if (url.includes("#")) {
-//   let filter = url.split("#")[1]; // is empty when there only is a # at the end of the url, which should not be happing, however we are safe anyways.
-//   alleRezepte.forEach(function(rezept) {
-//     if (filter === "cookbook"){
-//         rezept.style.display = "block";
-//     } else if (rezept.dataset.kathegorie !== filter) {
-//         rezept.style.display = "none";
-//     } else {
-//         rezept.style.display = "block";
-//     }
-//   });
-// }
-
-// btn_cookbook.click();
-
-//let targetId = localStorage.getItem("food_filter_id_from_index");
-
-// function foodFilter_from_index(){
-//     console.log(targetId);
-//     if (!targetId) {
-//         alleRezepte.forEach(function(rezept) {
-//             if (targetId === "Alle"){
-//                 rezept.style.display = "block";
-//             } else if (rezept.dataset.kathegorie != targetId) {
-//                 rezept.style.display = "none";
-//             } else {
-//                 rezept.style.display = "block";
-//             }
-//         });
-//     }
-// }
-
-// foodFilter(window.onload)
-
-//var rezeptArray = JSON.parse(localStorage.getItem("recipes"))
-//console.log(rezeptArray[0].type)
-
-
-
 let url =  window.location.href;
 let targetId;
 let counter = 0;
 
+// Läd die gefilterte Auswahl von der Index Seite
 if (url.includes("#") && counter == 0)  {
     targetId = url.split("#")[1];
-    //console.log(targetId) ;
     counter +=1;
     document.body.onload = addElement();
 }
 
-
-
-
-
+// Foodfilter Funktion die beim Klicken einer Auswahl durchläuft
 function foodFilter(event) {
     clearDivs()
     targetId = event.target.dataset.kathegorie; 
-    //console.log(targetId)
     addElement(targetId)
 }
 
+// Löscht alle bestehenden Divs in dem alle_rezepte Div aka id="div1"
 function clearDivs(){
     var existingDivs = document.getElementById("div1");
     while(existingDivs.firstChild){
         existingDivs.removeChild(existingDivs.firstChild)
-    }
-
-    
-    //var existingDivs = document.getElementsByClassName("rezept")
-    //var existingDivs2 = document.querySelector("rezept")
-    //console.log(existingDivs)
-    //console.log(existingDivs.length)
-    //console.log(existingDivs[0])
-    //delete existingDivs;
-
-    //for (var i=0; i=existingDivs.length; i++){
-        //existingDivs[i].parentNode.removeChild(existingDivs)
-        //delete existingDivs[i];
-        
-    //}
-        
-    //existingDivs.removeChild()
-    //existingDivs.parentNode.removeChild(existingDivs)
-    // HTMLCollection.prototype.remove = function() {
-    // [...document.querySelectorAll("rezept")].forEach(item => {
-    // if(item.parentElement) {
-    //     item.parentElement.removeChild(item);
-    // };
-    // });
-    // }
+        }
 }
 
+// Fügt Divs je nach Filterauswahl aus dem localStorage hinzu
 function addElement(){
     var alleRezepte = JSON.parse(localStorage.getItem("recipes"))
 
+    // Für den Button Alle Rezepte muss neuer Array mit allen Indizies erstellt werden. Ansonsten gilt targetId aus der foodFilter Funktion
     if(targetId == "Alle"){
         rezeptAuswahl = [];
         for(i=0;i<alleRezepte.length;i++){
             rezeptAuswahl.push(i)
-        }
+        } 
     } else {
         var rezeptAuswahl = JSON.parse(localStorage.getItem(targetId))
     }
     
-
-
     for (var i=0; i<rezeptAuswahl.length; i++){
-        //console.log(alleRezepte[rezeptAuswahl[i]])
-        var newDiv = document.createElement("div");
-        //var anchor = document.createElement("a");
+        var newDiv = document.createElement("div");        
         var foodImage = alleRezepte[rezeptAuswahl[i]].picture;
         Name = alleRezepte[rezeptAuswahl[i]].name;
         newDiv.style.backgroundImage = 'url("' + foodImage   + '")';
@@ -139,6 +68,8 @@ function addElement(){
         h3.appendChild(foodName)
         newDiv.appendChild(h3);
 
+        newDiv.setAttribute("onclick", "window.location.href = 'Rezept.html';") //works as well
+        //var anchor = document.createElement("a");
         //anchor.className = "rezept";
         //anchor.setAttribute("href", "../html/Rezept.html?ab=2")
         //anchor.style.background = "none"
@@ -146,17 +77,9 @@ function addElement(){
         //console.log(newDiv)
 
         document.getElementById("div1").appendChild(newDiv);
-
-        //var currentDiv = document.getElementById("div1");
-        //document.body.insertBefore(newDiv, currentDiv)
     }
 }
 
-//document.getElementsByClassName("rezept").setAttribute('onclick', 'location.href = /html/Rezept.html');
-
-//document.addEventListener("click", function(){
-//  getElementsByClassName("rezept") = link("/html/Rezept.html");
-//  });
 
 
 
