9b269ce708959cdf1814d2990b09ec1cb2294f62
donrigoyeah <46565056+donrigoyeah@users.noreply.github.com>
2020-04-28 14:43:17 +0200

    maybe 3 commit?
    div creation and deletion works.
    no anchor tags yet since luca is working on them

--- javascript/alleRezepte.js
+++ javascript/alleRezepte.js
@@ -3,6 +3,7 @@ const btn_cookbook = document.getElementById("cookbook" ),
       btn_first    = document.getElementById("first"    ),
       btn_dessert  = document.getElementById("dessert"  ),
       btn_drink    = document.getElementById("drink"    ),
+      btn_Alle     = document.getElementById("Alle"     ),
       btnAlleRezepte  = document.querySelectorAll(".rezept");
 
 btn_cookbook.addEventListener("click", foodFilter);
@@ -10,6 +11,7 @@ btn_main.    addEventListener("click", foodFilter);
 btn_first.   addEventListener("click", foodFilter);
 btn_dessert. addEventListener("click", foodFilter);
 btn_drink.   addEventListener("click", foodFilter);
+btn_Alle.    addEventListener("click", foodFilter);
 
 //LOAD.addEventlistener("onload", foodFilter);
 
@@ -59,7 +61,7 @@ let counter = 0;
 
 if (url.includes("#") && counter == 0)  {
     targetId = url.split("#")[1];
-    console.log(targetId) ;
+    //console.log(targetId) ;
     counter +=1;
     document.body.onload = addElement();
 }
@@ -70,48 +72,72 @@ if (url.includes("#") && counter == 0)  {
 
 function foodFilter(event) {
     clearDivs()
-
     targetId = event.target.dataset.kathegorie; 
     //console.log(targetId)
     addElement(targetId)
 }
 
 function clearDivs(){
-    //var existingDivs = document.getElementById("id").getElementsByTagName("div")
+    var existingDivs = document.getElementById("div1");
+    while(existingDivs.firstChild){
+        existingDivs.removeChild(existingDivs.firstChild)
+    }
+
+    
+    //var existingDivs = document.getElementsByClassName("rezept")
+    //var existingDivs2 = document.querySelector("rezept")
     //console.log(existingDivs)
+    //console.log(existingDivs.length)
+    //console.log(existingDivs[0])
+    //delete existingDivs;
+
+    //for (var i=0; i=existingDivs.length; i++){
+        //existingDivs[i].parentNode.removeChild(existingDivs)
+        //delete existingDivs[i];
+        
+    //}
+        
     //existingDivs.removeChild()
     //existingDivs.parentNode.removeChild(existingDivs)
-     HTMLCollection.prototype.remove = function() {
-     [...document.querySelectorAll("rezept")].forEach(item => {
-       if(item.parentElement) {
-           item.parentElement.removeChild(item);
-       };
-     });
-    }
+    // HTMLCollection.prototype.remove = function() {
+    // [...document.querySelectorAll("rezept")].forEach(item => {
+    // if(item.parentElement) {
+    //     item.parentElement.removeChild(item);
+    // };
+    // });
+    // }
 }
 
 function addElement(){
     var alleRezepte = JSON.parse(localStorage.getItem("recipes"))
-    var rezeptArray = JSON.parse(localStorage.getItem(targetId))
+
+    if(targetId == "Alle"){
+        rezeptAuswahl = [];
+        for(i=0;i<alleRezepte.length;i++){
+            rezeptAuswahl.push(i)
+        }
+    } else {
+        var rezeptAuswahl = JSON.parse(localStorage.getItem(targetId))
+    }
+    
 
 
-    
-    //console.log(alleRezepte[0].type)
-    //console.log(targetId)
-    console.log(rezeptArray)
-    
-    for (var i=0; i<rezeptArray.length; i++){
-        //console.log(alleRezepte[rezeptArray[i]])
+    for (var i=0; i<rezeptAuswahl.length; i++){
+        //console.log(alleRezepte[rezeptAuswahl[i]])
         var newDiv = document.createElement("div");
         //var anchor = document.createElement("a");
-        var foodImage = alleRezepte[rezeptArray[i]].picture;
-        Name = alleRezepte[rezeptArray[i]].name;
+        var foodImage = alleRezepte[rezeptAuswahl[i]].picture;
+        Name = alleRezepte[rezeptAuswahl[i]].name;
         newDiv.style.backgroundImage = 'url("' + foodImage   + '")';
-        newDiv.id = alleRezepte[rezeptArray[i]].id;
+        newDiv.style.backgroundSize = "cover";
+        newDiv.style.backgroundPosition = "center center";
+        newDiv.id = alleRezepte[rezeptAuswahl[i]].id;
         newDiv.className = "rezept";
-        newDiv.setAttribute("data-kathegorie", alleRezepte[rezeptArray[i]].type)
+        newDiv.setAttribute("data-kathegorie", alleRezepte[rezeptAuswahl[i]].type)
+        var h3 = document.createElement("H3")
         var foodName = document.createTextNode(Name)
-        newDiv.appendChild(foodName);
+        h3.appendChild(foodName)
+        newDiv.appendChild(h3);
 
         //anchor.className = "rezept";
         //anchor.setAttribute("href", "../html/Rezept.html?ab=2")
